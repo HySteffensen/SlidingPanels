99586c44f7bfd6b2d7b54ff9eca8e7ba90016939
HySteffensen <hyrum.steffensen@gmail.com>
2018-05-14 13:32:09 -0700

    04_02 panel lineup

--- includes/sliding_panels.js
+++ includes/sliding_panels.js
@@ -1,6 +1,14 @@
 /*  JavaScript Document                      */
 /*  Written by Chris Converse for lynda.com  */
+
 var panelWidth = 0;
 $(document).ready(function() {
   window.panelWidth = $('.sp').width();
+
+  $('.panel_container .panel').each(function(index) {
+    $(this).css({'width': window.panelWidth + 'px', 'left': (index * window.panelWidth)+'px'});
+
+    $('.sp .panels').css('width', (index+1) * window.panelWidth + 'px');
+  });
+
 });
