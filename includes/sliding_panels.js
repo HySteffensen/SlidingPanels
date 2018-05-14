d393c617a3705241509f905b409d25d605e32e25
HySteffensen <hyrum.steffensen@gmail.com>
2018-05-14 13:49:07 -0700

    04_03 tabclick

--- includes/sliding_panels.js
+++ includes/sliding_panels.js
@@ -6,9 +6,19 @@ $(document).ready(function() {
   window.panelWidth = $('.sp').width();
 
   $('.panel_container .panel').each(function(index) {
-    $(this).css({'width': window.panelWidth + 'px', 'left': (index * window.panelWidth)+'px'});
+    $(this).css({'width': window.panelWidth + 'px', 'left': (index * window.panelWidth) + 'px'});
 
-    $('.sp .panels').css('width', (index+1) * window.panelWidth + 'px');
+    $('.sp .panels').css('width', (index + 1) * window.panelWidth + 'px');
   });
+
+  $('.sp .tabs span').each(function() {
+
+    $(this).on('click', function() {
+      changePanels();
+    });
+
+  });
+
+  
 
 });
